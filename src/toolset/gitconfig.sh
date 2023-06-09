fde3b3037df5c61c6c1155a2280c43f166afd1b7
Ricardo Pereira <contato@ricardopedias.com.br>
2023-06-09 20:30:30 -0300

    Limpeza do projeto

--- src/toolset/gitconfig.sh
+++
@@ -1,3 +0,0 @@
-
-GIT_EMAIL="contato@ricardopedias.com.br"
-GIT_USERNAME="Ricardo Pereira"
--- src/toolset/setup_extern.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
-ROOT_DIR=$(dirname $SCRIPT_DIR)
-
-git clone https://github.com/google/googletest.git $ROOT_DIR/extern/googletest
-
